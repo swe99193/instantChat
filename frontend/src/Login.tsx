c7b92760b0f2adb7f1a965e77e8c7b0c949ad1f8
Kevin Hsiao <khsiao.33@gmail.com>
2023-07-30 12:12:08 +0800

    feat: update Login component fetch api to "application/json" Content-type

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -6,25 +6,30 @@ import "./Login.css";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL;
 
+
+// TODO: rewrite with MUI
+
 function Login() {
     const [errorMessages, setErrorMessages] = useState("");
     const [isSubmitted, setIsSubmitted] = useState(false);
     const [username, setUsername] = useState("");
 
-    // post form by fetch api
-    // ref: https://stackoverflow.com/questions/46640024/how-do-i-post-form-data-with-fetch-api
+
     const handleSubmit = async (event: React.FormEvent) => {
 
         event.preventDefault();
 
-        let formData = new FormData();
-        formData.append('username', (document.getElementById("username") as HTMLInputElement).value);
-        formData.append('password', (document.getElementById("password") as HTMLInputElement).value);
-
         const res = await fetch(`${BACKEND_URL}/login`, {
             method: "POST",
-            body: formData,
+            body: JSON.stringify({
+                username: (document.getElementById("username") as HTMLInputElement).value,
+                password: (document.getElementById("password") as HTMLInputElement).value
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
             credentials: "include"  // set cookie (session)
+
         });
 
         if (res.status == 200) {
@@ -54,7 +59,7 @@ function Login() {
                 </div>
                 <div className="error"> {errorMessages} </div>
                 <div className="button-container">
-                    <input type="submit" />
+                    <input type="submit" value="Sign In" />
                 </div>
             </form>
         </div>
