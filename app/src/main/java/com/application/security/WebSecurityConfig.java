e121f5f68ed7b040b0fc08db0c1ffa6e4a4e2ac5
Kevin Hsiao <khsiao.33@gmail.com>
2023-07-30 12:07:30 +0800

    feat: build custom login api, configure AuthenticationManager Bean
    - disable built-in login form
    - move PasswordEncoder from WebSecurityConfig to PasswordEncoderConfig
    - remove Thymeleaf (build.gradle)

--- app/src/main/java/com/application/security/WebSecurityConfig.java
+++ app/src/main/java/com/application/security/WebSecurityConfig.java
@@ -1,13 +1,23 @@
 package com.application.security;
 
+import com.application.registration.UserService;
+import net.devh.boot.grpc.server.security.authentication.BasicGrpcAuthenticationReader;
+import net.devh.boot.grpc.server.security.authentication.GrpcAuthenticationReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -38,18 +48,63 @@ public class WebSecurityConfig{
                 .authenticated()
                 .and()
                 .formLogin()
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .loginPage(frontendUrl+"login")
-                .loginProcessingUrl("/login")
+                .disable()      // avoid it overrides custom login api
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+//                .loginPage(frontendUrl+"/login")
+//                .loginProcessingUrl("/login")
 //                .defaultSuccessUrl(frontendUrl, true)     // disable this. redirect will cause CORS, unresolved
-                .failureHandler(authenticationFailureHandler());
+//                .permitAll()
+//                .failureHandler(authenticationFailureHandler())
+        ;
         return http.build();
     }
 
     @Bean
     public AuthenticationFailureHandler authenticationFailureHandler() {
         return new CustomAuthenticationFailureHandler();
+    }
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    AuthenticationManagerBuilder builder;
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        // How DaoAuthenticationProvider works:
+        // https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/dao-authentication-provider.html#servlet-authentication-daoauthenticationprovider
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+    // This code not working
+//    @Autowired
+//    AuthenticationManagerBuilder auth;
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager() throws Exception {
+//        return auth.authenticationProvider(new DaoAuthenticationProvider()).userDetailsService(userService)
+//                .passwordEncoder(bCryptPasswordEncoder).and().build();
+//    }
+
+
+    // Note: need this bean for gRPC
+    @Bean
+    public GrpcAuthenticationReader grpcAuthenticationReader() {
+        return new BasicGrpcAuthenticationReader();
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
     }
 
     // ref: https://docs.spring.io/spring-security/reference/reactive/integrations/cors.html
@@ -64,8 +119,5 @@ public class WebSecurityConfig{
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+
 }
