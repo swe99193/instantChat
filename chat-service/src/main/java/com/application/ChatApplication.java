4576808d49653f4ccf895e461c9fb53f4145bf79
Kevin Hsiao <khsiao.33@gmail.com>
2023-08-14 23:57:32 +0800

    feat(backend:chat-service): rename app ➡️ chat-service

--- chat-service/src/main/java/com/application/ChatApplication.java
+++ chat-service/src/main/java/com/application/ChatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessagingStompWebsocketApplication {
+public class ChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+		SpringApplication.run(ChatApplication.class, args);
 		System.out.println("✅ Application successfully launched 🚀🚀");
 	}
 }
