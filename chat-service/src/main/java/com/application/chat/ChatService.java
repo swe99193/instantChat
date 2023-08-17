f2c502226c2482f5648901427c9dfb988b1b41bb
Kevin Hsiao <khsiao.33@gmail.com>
2023-08-17 23:42:20 +0800

    feat(chat-service): merge message-storage-service

--- chat-service/src/main/java/com/application/chat/ChatService.java
+++ chat-service/src/main/java/com/application/chat/ChatService.java
@@ -3,90 +3,56 @@ package com.application.chat;
 import ChannelMappingServiceLib.ChannelMappingServiceGrpc.ChannelMappingServiceBlockingStub;
 import ChannelMappingServiceLib.findChannelIdRequest;
 import ChannelMappingServiceLib.findChannelIdResponse;
-import MessageServiceLib.ListMessageResponse;
-import MessageServiceLib.ListMessageRequest;
-import MessageServiceLib.MessageServiceGrpc.MessageServiceBlockingStub;
-import MessageServiceLib.SaveMessageResponse;
-import MessageServiceLib.SaveMessageRequest;
 import com.application.message_storage.Message;
+import com.application.message_storage.MessageService;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ChatService {
     // FIXME: gRPC client cannot be Autowired
 
-    @GrpcClient("grpc-server-message")
-    private MessageServiceBlockingStub messageService;
-
     @GrpcClient("grpc-server-channel-mapping")
     private ChannelMappingServiceBlockingStub channelMappingService;
 
-//    @Autowired
-//    public ChatService(MessageServiceBlockingStub messageService, ChannelMappingServiceBlockingStub channelMappingService) {
-//        this.messageService = messageService;
-//        this.channelMappingService = channelMappingService;
-//    }
+    private final MessageService messageService;
+
+
+    @Autowired
+    public ChatService(MessageService messageService) {
+        this.messageService = messageService;
+    }
 
     public void saveMessage(String sender, String receiver, String content){
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 //        String channel_id = channelMappingService.findChannelId(sender, receiver);
 
-//        Message message = new Message(channel_id, timestamp.getTime(), sender, receiver, "text", content);
-
-        // gRPC
+        // gRPC, get channel_id
         findChannelIdRequest _req = findChannelIdRequest.newBuilder().setUser1(sender).setUser2(receiver).build();
         findChannelIdResponse _res = channelMappingService.findChannelId(_req);
         String channel_id = _res.getChannelId();
 
-        // gRPC
-        MessageServiceLib.Message message = MessageServiceLib.Message.newBuilder()
-                .setChannelId(channel_id)
-                .setTimestamp(timestamp.getTime())
-                .setSender(sender)
-                .setReceiver(receiver)
-                .setContentType("text")
-                .setContent(content)
-                .build();
-        SaveMessageRequest req = SaveMessageRequest.newBuilder().setMessage(message).build();
-        SaveMessageResponse res = messageService.saveMessage(req);
-        boolean isSuccess = res.getIsSuccess();
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        Message message = new Message(channel_id, timestamp.getTime(), sender, receiver, "text", content);
+
+        messageService.saveMessage(message);
     }
 
     public List<Message> listMessage(String sender, String receiver){
 
 //        String channel_id = channelMappingService.findChannelId(sender, receiver);
 
-        // gRPC
+        // gRPC, get channel_id
         findChannelIdRequest _req = findChannelIdRequest.newBuilder().setUser1(sender).setUser2(receiver).build();
         findChannelIdResponse _res = channelMappingService.findChannelId(_req);
         String channel_id = _res.getChannelId();
 
-        // gRPC
-        ListMessageRequest req = ListMessageRequest.newBuilder().setChannelId(channel_id).build();
-        ListMessageResponse res = messageService.listMessage(req);
-        List<MessageServiceLib.Message> mList = res.getMessageList();
-
-        // convert to Message Class
-        List<Message> messageList = new ArrayList<Message>();
-        for (MessageServiceLib.Message m: mList) {
-            Message message = new Message(
-                    m.getChannelId(),
-                    m.getTimestamp(),
-                    m.getSender(),
-                    m.getReceiver(),
-                    m.getContentType(),
-                    m.getContent());
-            messageList.add(message);
-        }
+        List<Message> messageList = messageService.listMessage(channel_id);
 
         return messageList;
-//		return new ArrayList<Message>(); // test: empty list
     }
 
 }
---
+++ chat-service/src/main/java/com/application/message_storage/MessageService.java
@@ -0,0 +1,49 @@
+package com.application.message_storage;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+// Dynamodb sdk ref:
+// https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/examples-dynamodb-items.html
+// https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBMapper.QueryScanExample.html
+
+@Service
+public class MessageService {
+
+    private final AmazonDynamoDB client;
+
+    @Autowired
+    public MessageService(AmazonDynamoDB client) {
+        this.client = client;
+    }
+
+    public void saveMessage(Message message) {
+        DynamoDBMapper mapper = new DynamoDBMapper(client);
+        mapper.save(message);
+    }
+
+    /**
+     * query the database based on the partition key "channel id"
+     * */
+    public List<Message> listMessage(String channel_id){
+        DynamoDBMapper mapper = new DynamoDBMapper(client);
+
+        Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+        eav.put(":val1", new AttributeValue().withS(channel_id));
+
+        DynamoDBQueryExpression<Message> queryExpression = new DynamoDBQueryExpression<Message>().withKeyConditionExpression("channel_id = :val1")
+                .withExpressionAttributeValues(eav);
+
+        // Note: mapper.query has support for pagination
+        return mapper.query(Message.class, queryExpression);
+    }
+}
